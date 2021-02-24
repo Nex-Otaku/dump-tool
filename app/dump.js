9888ebd076ae030107c06499c427fbe5c0f9e7f1
Nex Otaku <nex@otaku.ru>
2021-02-24 11:03:41 +0300

    Обновил команду выгрузки дампа

--- app/dump.js
+++ app/dump.js
@@ -186,7 +186,7 @@ const dumpData = async () => {
     const dumpFilePath = files.getTempFilePath(dumpFileName);
 
     // Была настройка --set-gtid-purged=OFF, пришлось убрать, т.к. валится с ошибкой unknown variable 'set-gtid-purged=OFF'
-    const dumpCommand = 'mysqldump --no-create-info -u {user} -p"{password}" -h {host} --port {port} --single-transaction --skip-lock-tables --default-character-set=utf8mb4 --hex-blob --max-allowed-packet=512000000 {db} {table} > {dumpFilePath}';
+    const dumpCommand = 'mysqldump --complete-insert --no-create-info -u {user} -p"{password}" -h {host} --port {port} --single-transaction --skip-lock-tables --default-character-set=utf8mb4 --hex-blob --max-allowed-packet=512000000 {db} {table} > {dumpFilePath}';
     const dumpCommandParametrized = lib.parametrize(dumpCommand, {
         host: remote.host,
         port: remote.port,
