0475c2d2d520195f05c575b40177c63b06e76543
Nex Otaku <nex@otaku.ru>
2021-02-24 11:02:37 +0300

    Привязал на 127.0.0.1, так как WSL плохо работает с localhost

--- app/mysql.js
+++ app/mysql.js
@@ -7,7 +7,7 @@ module.exports = {
 
     getLocalConnectionCredentials: () => {
         return {
-            host: 'localhost',
+            host: '127.0.0.1',
             port: 3306,
             username: 'root',
             password: ''
